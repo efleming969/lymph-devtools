2b3922493a51e07cba3a5466843b7ef5809e2da4
Erick Fleming <efleming969@gmail.com>
2018-03-14 09:31:19 -0500

    refactor(client-deploy): change s3 mime lookup
    when using node-mime module typescript was complaining so implmented a
    primitive mime lookup to avoid additional research time and complexity

--- src/Clients.ts
+++ src/Clients.ts
@@ -2,7 +2,6 @@ import * as FS from "fs-extra"
 import * as Path from "path"
 import * as AWS from "aws-sdk"
 import * as Glob from "globby"
-import * as Mime from "mime"
 
 import * as Scripts from "./Scripts"
 import * as Styles from "./Styles"
@@ -25,8 +24,31 @@ export type Module = {
     name: string,
     title: string,
     styles: string[],
+
     scripts: ModuleScript[],
     globals: any
+}
+
+/**
+ * created this primitive mime lookup, due to an issue with node-mime library
+ * and jest's resolver.  Found a discussion https://github.com/kulshekhar/ts-jest/issues/269
+ * related to this, but seemed overly complicated for what was needed
+ */
+const getMimeFromFile = function ( file_path: string ): string {
+    const mime_types = {
+        ".gif": "image/gif",
+        ".jpeg": "image/jpeg",
+        ".png": "image/png",
+        ".svg": "image/svg+xm",
+        ".ico": "image/x-icon",
+        ".css": "text/css",
+        ".html": "text/html",
+        ".js": "application/javascript"
+    }
+
+    const file_extension = Path.extname( file_path )
+
+    return mime_types[ file_extension ]
 }
 
 export const detectModules = function ( config: Config ): Promise<Module[]> {
@@ -65,7 +87,7 @@ export const deploy = function ( source: string, target: string, region: string 
                     Body: buffer,
                     Bucket: target,
                     Key: file.replace( source + "/", "" ),
-                    ContentType: Mime.getType( file ), // content-type is needed since S3 is bad at guessing mime types
+                    ContentType: getMimeFromFile( file ), // content-type is needed since S3 does not guess mime types
                     CacheControl: "no-cache,public,max-age=60"
                 }
                 console.log( "putting", put_config.Bucket, put_config.Key )
